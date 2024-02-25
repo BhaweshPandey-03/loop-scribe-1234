35d6f6d0f8e9003120ee725168bd511ab3f13e7b
Skchouhan753 <s.k.chouhan753@gmail.com>
2024-02-25 23:33:32 +0530

    update

--- khan-academy/src/App.jsx
+++ khan-academy/src/App.jsx
@@ -1,17 +1,25 @@
+import "./App.css";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 
+<<<<<<< Updated upstream
 // import './App.css'
 import { useEffect, useState } from 'react'
 import { AllRoutes } from './AllRoutes/AllRoutes';
 import Navbar from './Components/Navbar/Navbar';
+=======
+import { BrowserRouter } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { ChakraProvider } from "@chakra-ui/react";
+import AllRoutes from "./Allroutes/MainRoute";
+
+>>>>>>> Stashed changes
 
 function App() {
-  const current_theme = localStorage.getItem('current_theme');
-  const [theme,setTheme] = useState(current_theme?current_theme:'light');
-  useEffect(()=>{
-    localStorage.setItem('current_theme',theme)
-  },[theme])
-
   return (
+<<<<<<< Updated upstream
     <>
     <div className={`container ${theme}`}>
       <Navbar theme={theme} setTheme={setTheme}/>
@@ -19,6 +27,23 @@ function App() {
     </div>
     </>
   )
+=======
+    <Provider store={store}>
+      <BrowserRouter>
+        <ChakraProvider>
+          <div className="App">
+            <Navbar />
+
+            <AllRoutes />
+
+            <Footer />
+        
+          </div>
+        </ChakraProvider>
+      </BrowserRouter>
+    </Provider>
+  );
+>>>>>>> Stashed changes
 }
 
-export default App
+export default App;
